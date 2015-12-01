2531a0626ad3a32a76579629ed1952892a237552
atifar <tyatya68@gmail.com>
2015-12-01 14:30:45 -0800

    Basic view now returns minimal HTML.

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
