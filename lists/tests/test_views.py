72a3c122f4df6f2eec389b2afbccdff8edbb5d64
atifar <tyatya68@gmail.com>
2015-12-15 14:28:23 -0800

    Augment form to handle Item uniqueness validation.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,6 +3,7 @@ from django.utils.html import escape
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
+from unittest import skip
 
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
@@ -134,6 +135,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("you've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
