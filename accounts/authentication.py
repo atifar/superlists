d7c0346027e55d7e74377f9774e0f2f1d9359821
atifar <tyatya68@gmail.com>
2015-12-26 16:48:46 -0800

    Implement logging in authentication. Fabric deployment is BROKEN.

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,9 +1,11 @@
+import logging
 import requests
 from django.conf import settings
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
+logger = logging.getLogger(__name__)
 
 
 class PersonaAuthenticationBackend(object):
@@ -19,6 +21,10 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning(
+                'Persona says no. Json was: {}'.format(response.json())
+            )
 
     def get_user(self, email):
         try:
