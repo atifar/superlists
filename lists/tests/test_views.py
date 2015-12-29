e83a7bb12dcb5f469ab7536ba65aea9306c18943
atifar <tyatya68@gmail.com>
2015-12-28 16:06:33 -0800

    URL, placeholder view, and first-cut templates for my_lists.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -152,3 +152,10 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
+
+
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
